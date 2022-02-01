dcf8c5d82152cbefd5e93b502e299d9a606c74ab
Timofey <timofey.lazarev@gmail.com>
2022-02-01 22:53:53 +0300

    Сконфигурировать Kotlin в новом проекте Перевести MainActivity на Kotlin.

--- app/src/main/java/com/example/gb_first/MainActivity.kt
+++ app/src/main/java/com/example/gb_first/MainActivity.kt
@@ -1,14 +1,12 @@
-package com.example.gb_first;
+package com.example.gb_first
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gb_first.R
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
