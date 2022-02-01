29b02d9aec4c044e2d8fdefcca5a78dbe437d050
Timofey <timofey.lazarev@gmail.com>
2022-02-01 23:53:04 +0300

    Сформировать data class с двумя свойствами и вывести их на экран приложения

--- app/src/main/java/com/example/gb_first/MainActivity.kt
+++ app/src/main/java/com/example/gb_first/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.gb_first.R
 
@@ -13,10 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Бдыщ", Toast.LENGTH_SHORT).show()
-        }
+        val student = Human("Тима", 18)
+        val name: TextView = findViewById(R.id.name) as TextView
+        val age: TextView = findViewById(R.id.age) as TextView
+        name.text = student.name
+        age.text = student.age.toString()
+
     }
 
+    data class Human (var name:String, val age:Int)
 }
